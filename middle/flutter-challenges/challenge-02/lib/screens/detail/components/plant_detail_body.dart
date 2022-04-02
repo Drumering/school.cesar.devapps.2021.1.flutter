965f3f484899f9f5b76c72fc62a8fc1481c7010b
Anderson Mendes de Almeida <andrums.amda@gmail.com>
2022-04-02 16:38:05 -0300

    [TASK]Small adjustments to run the project

--- middle/flutter-challenges/challenge-02/lib/screens/detail/components/plant_detail_body.dart
+++ middle/flutter-challenges/challenge-02/lib/screens/detail/components/plant_detail_body.dart
@@ -11,36 +11,30 @@ class PlantDetailBody extends StatelessWidget {
       children: [
         Row(
           children: [
-            Expanded(
-              child: Column()
-            ),
+            Expanded(child: Column()),
             Container(
               height: size.height * 0.6,
               width: size.width * 0.75,
               decoration: BoxDecoration(
                 borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(63),
-                  bottomLeft: Radius.circular(63)
-                ),
+                    topLeft: Radius.circular(63),
+                    bottomLeft: Radius.circular(63)),
                 boxShadow: [
                   BoxShadow(
-                    offset: const Offset(0, 10),
-                    blurRadius: 60,
-                    color: kPrimaryColor.withOpacity(0.29)
-                  )
+                      offset: const Offset(0, 10),
+                      blurRadius: 60,
+                      color: kPrimaryColor.withOpacity(0.29))
                 ],
                 image: const DecorationImage(
                   image: AssetImage("assets/images/img.png"),
                   fit: BoxFit.cover,
                   alignment: Alignment.centerLeft,
-                ),  
+                ),
               ),
             ),
           ],
         ),
-        
         const Spacer(),
-        
         Row(
           children: [
             SizedBox(
@@ -49,18 +43,13 @@ class PlantDetailBody extends StatelessWidget {
               child: ElevatedButton(
                 child: const Text("Buy Now"),
                 style: ElevatedButton.styleFrom(
-                  primary: kPrimaryColor,
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(20)
-                    )
-                  )
-                ),
-                onPressed: () {  },
+                    primary: kPrimaryColor,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.only(topRight: Radius.circular(20)))),
+                onPressed: () {},
               ),
             ),
-            
-
           ],
         )
       ],
